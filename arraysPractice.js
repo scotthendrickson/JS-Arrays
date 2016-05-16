8f9f5f65cad93febd4fbbc8eeefa23cd70fbd660
Scott Hendrickson <Scott Hendrickson>
2016-05-16 15:46:51 -0600

    Completed

--- arraysPractice.js
+++ arraysPractice.js
@@ -101,10 +101,19 @@ var getRandomArbitrary = function() {
 // var numbers = [0,3,4,5,6,7,9,14,17,24,25,26,29,30];
 //Above you're given a function that will return a random number between 0 and 30.  There is also a commented out array full of numbers to help you visualize what your function will be receiving.
 
-// Your job is to write a function named finder that will get a random number (by invoking getRandomArbitrary), then loop through the array (that will be passed in as a parameter) to see if that random number is in the array. If it is, return true, if it's not, return false
+// Your job is to write a function named finder that will get a random number (by invoking getRandomArbitrary), then loop through the array (that will be passed in as a parameter) to see if that random number is in the array.
+//If it is, return true, if it's not, return false
 
   //Code Here
-
+function finder(arr) {
+  var num = getRandomArbitrary();
+  var index = arr.indexOf(num);
+  if (index !== -1) {
+    return true;
+  }else {
+    return false;
+  }
+}
   //Code Here
 
 
@@ -116,7 +125,9 @@ var str = 'this is my sentence';
 //Write a function called reverse that takes a given str as it's only argument and returns that string after it's been reversed
 
   //Code Here
-
+function reverse(str) {
+  return str.split("").reverse().join("");
+}
 
 //Next Problem
 
@@ -138,6 +149,27 @@ var myGroceryList = ['chips', 'pizza', 'hotpockets', 'MtnDew', 'corndogs'];
 */
 
   //Code Here
+function removeItem(arr, item) {
+  if (arguments.length === 0) {
+    return [];
+  }
+  var index = arr.indexOf(item);
+  if (index !== -1) {
+    arr.splice(index, 1);
+  }
+  return arr;
+}
+
+function addItem(arr, item) {
+  if (arguments.length === 0) {
+    return [];
+  }
+  var index = arr.indexOf(item);
+  if (index === -1) {
+    arr.push(item);
+  }
+  return arr;
+}
 
 //removeItem(myGroceryList, 'chips') --> ['pizza', 'hotpockets', 'MtnDew', 'corndogs'];
 //addItem(myGroceryList, 'Jerky') --> ['pizza', 'hotpockets', 'MtnDew', 'corndogs', 'Jerky'];
@@ -151,7 +183,13 @@ var myGroceryList = ['chips', 'pizza', 'hotpockets', 'MtnDew', 'corndogs'];
 //Write a function called maker that creates an array, fills that array with numbers from 1 to 215, then returns the array.
 
   //Code Here
-
+function maker() {
+  var nums = [];
+  for (var i = 1; i <= 215; i++) {
+    nums.push(i);
+  }
+  return nums;
+}
 
 
 //Next Problem
@@ -162,7 +200,13 @@ var numbers = [5, '9', 16, 19, '25', '34', 48];
 //array after adding ten to each item in numbers. *Verify your answer is correct. --> [15, 19, 26, 29, 35, 44, 58]
 
   //Code Here
-
+function addTen(arr) {
+  var result = [];
+  for (var i =0; i < arr.length; i++) {
+    result.push(parseInt(arr[i]) + 10);
+  }
+  return result;
+}
 
 
 //Next Problem
@@ -183,7 +227,13 @@ for(var i = 0; i < num2; i++){
 //Write a function called 'longer' that is given arr1 and arr2 as it's only arguments. Return the array which is longest.
 
   //Code Here
-
+function longer(nums1, nums2) {
+  if (nums1.length > nums2.length) {
+    return nums1;
+  }else {
+    return nums2;
+  }
+}
 
 /*As a continuation of the previous problem, write another function called 'both'.
   Your 'both' function will be given two arguments, arr1 and arr2 (from the previous example).
@@ -192,7 +242,17 @@ for(var i = 0; i < num2; i++){
 */
 
   //Code Here
-
+  function both(arg1, arg2) {
+  	var same = [];
+  	for (var i = 0; i < arg1.length; i++) {
+  		for (var x = 0; x < arg2.length; x++) {
+  			if (arg1[i] === arg2[x]) {
+  				same.push(arg1[i]);
+  			}
+  		}
+  	}
+  	return same;
+  }
 
 
 
@@ -232,12 +292,36 @@ array with those four objects. After that console.log the length of the Array an
 sure that it's equal to 4. */
 
   //Code Here
+  devMountainEmployees = [tyler = {
+      name: 'Tyler',
+      position: 'Lead Instructor/Engineer',
+      spiritAnimal: 'Honey Badger'
+  },
+  cahlan = {
+      name: 'Cahlan',
+      position: 'CEO',
+      spiritAnimal: 'butterfly'
+  },
+  ryan = {
+      name: 'Ryan',
+      position: 'Marketing',
+      spiritAnimal: 'fox'
+  },
+  colt = {
+      name: 'Colt',
+      position: 'Everything really',
+      spiritAnimal: 'Young Male Horse'
+  }];
 
 /*Now let's say Cahlan has a mental breakdown and has to take a leave of absence to 'find himself'.
 Loop through your devMountainEmployees until you find cahlan, then remove him from the array.*/
 
   //Code Here
-
+for (var i = 0; i < devMountainEmployees.length; i++) {
+  if( devMountainEmployees[i].name === "Cahlan") {
+    devMountainEmployees.splice(i,1);
+  }
+}
 
 
 
@@ -279,6 +363,7 @@ of Data is to have an Array full of objects. */
 //Create an empty array called users.
 
   //Code Here
+var users = [];
 
 /*Now add three user objects to your users array. Each user object should contain the
 following properties. name, email, password, username.*/
@@ -292,7 +377,26 @@ var user1 = {
 };
 
 //Your Code Here
-
+users = [
+  user1 = {
+      name: 'Tyler McGinnis',
+      email: 'tylermcginnis33@gmail.com',
+      password: 'iLoveJavaScript',
+      username: 'infiniateLoop'
+  },
+  user2 = {
+      name: 'b',
+      email: 'b',
+      password: 'b',
+      username: 'b'
+  },
+  user3 = {
+      name: 'c',
+      email: 'c',
+      password: 'c',
+      username: 'c'
+  }
+]
 /*Now you have a very common data structure. Twitter is a good use case.
 It's easy to imagine that your followers list on Twitter is an Array full or objects
 and those objects contain properties about the specific person you follow.*/
@@ -302,5 +406,9 @@ objects until you find Tyler's account (use tylermcginnis33@gmail.com to find hi
 Once you find the particular index he's located in, delete him from the array.*/
 
   //Code Here
-
+for (var i = 0; i < users.length; i++) {
+  if( users[i].email === "tylermcginnis33@gmail.com") {
+    users.splice(i,1);
+  }
+}
 //The activity we just did is very much how data works in 'the real world'.
